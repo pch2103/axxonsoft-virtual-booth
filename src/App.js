bc3637a0388aaae31eb145db67d4ac5973934a7c
pch2013 <pch2103@gmail.com>
2021-01-08 20:16:05 +0300

    First WP

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import "./assets/styles/App.scss";
 
 import {Canvas} from "react-three-fiber";
 import {OrbitControls} from "@react-three/drei";
+import {softShadows} from "@react-three/drei";
 import Lights from "./components/Three/lights";
 import Model from "./components/Three/VirtualWP";
+import Floor from "./components/Three/Floor";
+
 
 const App = () => {
+  softShadows(undefined);
   return (
     <>
       <Canvas
@@ -15,11 +19,12 @@ const App = () => {
         shadowMap
         camera={{position:[5,4,3], fov: 20}}
       >
+        <Lights/>
         <Suspense fallback={null}>
-          <Lights/>
           <Model position = {[0, -1, 0]}/>
+          <Floor />
         </Suspense>
-        <OrbitControls/>
+        <OrbitControls maxPolarAngle={Math.PI/2} />
       </Canvas>
     </>
   );
