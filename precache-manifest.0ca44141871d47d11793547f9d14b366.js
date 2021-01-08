60cf78e72b8ec0001865e25eb26644858bbea156
pch2013 <pch2103@gmail.com>
2021-01-08 16:37:34 +0300

    Updates

--- precache-manifest.0ca44141871d47d11793547f9d14b366.js
+++ precache-manifest.0ca44141871d47d11793547f9d14b366.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "723c6f91b3445a58248ae97f2471b07e",
+    "revision": "da3dc957df7fbb9b6fab9ccf6b8be0a4",
     "url": "/axxonsoft-virtual-booth/index.html"
   },
   {
-    "revision": "e4f103162a6c287a271b",
+    "revision": "f4004d09a7501d9c1f95",
     "url": "/axxonsoft-virtual-booth/static/css/main.2d4a66a9.chunk.css"
   },
   {
-    "revision": "d7f61783698eeb691803",
-    "url": "/axxonsoft-virtual-booth/static/js/2.1067344e.chunk.js"
+    "revision": "4ef1fe7a1af54a63925c",
+    "url": "/axxonsoft-virtual-booth/static/js/2.3ee5605f.chunk.js"
   },
   {
     "revision": "931bbbbedd5814f855e37a94c13b08fe",
-    "url": "/axxonsoft-virtual-booth/static/js/2.1067344e.chunk.js.LICENSE.txt"
+    "url": "/axxonsoft-virtual-booth/static/js/2.3ee5605f.chunk.js.LICENSE.txt"
   },
   {
     "revision": "6503c0b4840854dbcbf7",
     "url": "/axxonsoft-virtual-booth/static/js/3.e04c41b5.chunk.js"
   },
   {
-    "revision": "e4f103162a6c287a271b",
-    "url": "/axxonsoft-virtual-booth/static/js/main.7f53a95d.chunk.js"
+    "revision": "f4004d09a7501d9c1f95",
+    "url": "/axxonsoft-virtual-booth/static/js/main.b4898cb8.chunk.js"
   },
   {
     "revision": "3c0c5585e32514a73770",
