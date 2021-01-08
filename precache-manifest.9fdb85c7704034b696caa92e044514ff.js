850f746299d1ff6f46ff6c9db20ffa1580fec161
pch2013 <pch2103@gmail.com>
2021-01-08 18:11:51 +0300

    Updates

---
+++ precache-manifest.9fdb85c7704034b696caa92e044514ff.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8af3bfe7a4d37e22a84e2081fdd67a64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3517eb08fd2cc8af962f",
+    "url": "/static/css/main.3a14162a.chunk.css"
+  },
+  {
+    "revision": "4ef1fe7a1af54a63925c",
+    "url": "/static/js/2.3ee5605f.chunk.js"
+  },
+  {
+    "revision": "931bbbbedd5814f855e37a94c13b08fe",
+    "url": "/static/js/2.3ee5605f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6503c0b4840854dbcbf7",
+    "url": "/static/js/3.e04c41b5.chunk.js"
+  },
+  {
+    "revision": "3517eb08fd2cc8af962f",
+    "url": "/static/js/main.265b7d3f.chunk.js"
+  },
+  {
+    "revision": "c5ac82dd7ca1cafcaaf4",
+    "url": "/static/js/runtime-main.008cc99d.js"
+  },
+  {
+    "revision": "2f51da83df5099ce5abe03fb4dba1a54",
+    "url": "/static/media/1_Minecraft-Regular.2f51da83.ttf"
+  },
+  {
+    "revision": "a215ea6e21d7f468b525420c7d35412c",
+    "url": "/static/media/1_Minecraft-Regular.a215ea6e.woff"
+  },
+  {
+    "revision": "a2e8f05433d3e0e4571be990880cc5e3",
+    "url": "/static/media/1_Minecraft-Regular.a2e8f054.svg"
+  },
+  {
+    "revision": "e55aa7ec649a62fd4457ed08238cdcd7",
+    "url": "/static/media/1_Minecraft-Regular.e55aa7ec.eot"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/axxonsoft-virtual-booth/precache-manifest.0ca44141871d47d11793547f9d14b366.js"
+  "/precache-manifest.9fdb85c7704034b696caa92e044514ff.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/axxonsoft-virtual-booth/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
