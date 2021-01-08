04880eb0ca4cc7097d17d1d34fb52c12986ed83e
pch2013 <pch2103@gmail.com>
2021-01-08 20:10:49 +0300

    First WP

--- src/components/Three/MonitorWP.js
+++ src/components/Three/MonitorWP.js
@@ -3,12 +3,19 @@ import { TextureLoader } from 'three'
 import { useLoader } from 'react-three-fiber'
 
 export default function MonitorWP(props) {
-	const mesh = useRef()
-	//const texture_1 = useLoader(TextureLoader, props.texture);
+	const group = useRef()
+	const texture_1 = useLoader(TextureLoader, props.texture);
+
 	return (
-				<mesh position={props.position} ref={mesh}>
-					<boxBufferGeometry attach="geometry"args={[0.6, 0.35, 0.01]} />
+			<group ref={group} rotation={props.rotation}>
+				<mesh position={props.position} >
+					<boxBufferGeometry attach="geometry" args={[0.6, 0.4, 0.01]} />
 					<meshStandardMaterial attach="material"map={texture_1} />
 				</mesh>
+				<mesh position={[props.position[0], props.position[1], props.position[2]-0.007]}>
+					<boxBufferGeometry attach="geometry" args={[0.62, 0.42, 0.02]} />
+					<meshBasicMaterial color = "black"/>
+				</mesh>
+			</group>
 	);
 }
