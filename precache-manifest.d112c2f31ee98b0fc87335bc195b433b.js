6eaaa09df3a6208b4c2438c0d6e7ed75f1d2a673
pch2013 <pch2103@gmail.com>
2021-01-08 14:58:08 +0300

    Updates

---
+++ precache-manifest.d112c2f31ee98b0fc87335bc195b433b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "723c6f91b3445a58248ae97f2471b07e",
+    "url": "/axxonsoft-virtual-booth/index.html"
+  },
+  {
+    "revision": "e4f103162a6c287a271b",
+    "url": "/axxonsoft-virtual-booth/static/css/main.2d4a66a9.chunk.css"
+  },
+  {
+    "revision": "d7f61783698eeb691803",
+    "url": "/axxonsoft-virtual-booth/static/js/2.1067344e.chunk.js"
+  },
+  {
+    "revision": "931bbbbedd5814f855e37a94c13b08fe",
+    "url": "/axxonsoft-virtual-booth/static/js/2.1067344e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6503c0b4840854dbcbf7",
+    "url": "/axxonsoft-virtual-booth/static/js/3.e04c41b5.chunk.js"
+  },
+  {
+    "revision": "e4f103162a6c287a271b",
+    "url": "/axxonsoft-virtual-booth/static/js/main.7f53a95d.chunk.js"
+  },
+  {
+    "revision": "3c0c5585e32514a73770",
+    "url": "/axxonsoft-virtual-booth/static/js/runtime-main.ce7e16c1.js"
+  },
+  {
+    "revision": "2f51da83df5099ce5abe03fb4dba1a54",
+    "url": "/axxonsoft-virtual-booth/static/media/1_Minecraft-Regular.2f51da83.ttf"
+  },
+  {
+    "revision": "a215ea6e21d7f468b525420c7d35412c",
+    "url": "/axxonsoft-virtual-booth/static/media/1_Minecraft-Regular.a215ea6e.woff"
+  },
+  {
+    "revision": "a2e8f05433d3e0e4571be990880cc5e3",
+    "url": "/axxonsoft-virtual-booth/static/media/1_Minecraft-Regular.a2e8f054.svg"
+  },
+  {
+    "revision": "e55aa7ec649a62fd4457ed08238cdcd7",
+    "url": "/axxonsoft-virtual-booth/static/media/1_Minecraft-Regular.e55aa7ec.eot"
+  }
+]);
