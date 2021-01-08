8af258fa4c04c9cb0fd18750c2dd1500184678b6
pch2013 <pch2103@gmail.com>
2021-01-08 18:07:06 +0300

    First

--- src/components/Three/MonitorWP.js
+++ src/components/Three/MonitorWP.js
@@ -4,7 +4,7 @@ import { useLoader } from 'react-three-fiber'
 
 export default function MonitorWP(props) {
 	const mesh = useRef()
-	const texture_1 = useLoader(TextureLoader, props.texture);
+	//const texture_1 = useLoader(TextureLoader, props.texture);
 	return (
 				<mesh position={props.position} ref={mesh}>
 					<boxBufferGeometry attach="geometry"args={[0.6, 0.35, 0.01]} />
